459901ff6f76aaf6f06a6919b57845232a8283b6
QueenOfShunpos <69691726+matthew1i@users.noreply.github.com>
2024-05-10 10:03:52 -0400

    soy estudioso

---
+++ Retos con decimas/reto_1.py
@@ -0,0 +1,4 @@
+nombre = input("como te llamas: ")
+edad = int(input("que edad tienes: "))
+años_sumados = edad + nombre
+print("hola mi nombre es (nombre) y tengo (edad) años pero en 20 años tendre (años_sumados) años")
---
+++ tipos_de_numeros.py
@@ -0,0 +1,54 @@
+edad = 19
+estatura = 1.80
+n_comp = 5 + 4
+
+
+
+
+
+
+
+
+
+estatura = 1.70
+peso = 70
+imc = peso / (estatura **2)
+print(imc)
+
+institucion = "universidad de los lagos"
+asignatura = "progamacion"
+descripcion = "asignatura de primer semestre de la carrera de ingieneria civil en informatica"
+#Imprimir la posicion de un caracter
+print (institucion[-1])
+#Concatenacion
+resultado = print(institucion + " " + asignatura)
+#multiplicacion de texto por un numero
+salida = print(institucion * 4)
+#Utilizando la variable split en la string 
+print(institucion.split())
+#Len cuenta las palabras
+print(len(institucion))
+#booleanos
+ampolleta = False
+interruptor = True
+print(type(ampolleta),type(interruptor))
+print(type(imc))
+print(type(peso))
+#comparaciones booleanas
+print(1 <= 0)
+print(100 >= 10)
+print(19 == 19)
+#funcion bool
+print(bool(0))
+print(bool(1))
+#listas
+ciudades = ["castro", "ancud", "quellon", "chonchi"]
+varios = ["nicolas", 20,True]
+print(type(ciudades))
+print(type(varios))
+list("python", "ruby")
+print(ciudades)
+
+
+
+
