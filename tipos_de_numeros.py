4003901b8b877807a1adceee19d3cd330a05379f
QueenOfShunpos <69691726+matthew1i@users.noreply.github.com>
2024-05-31 10:07:37 -0400

    a

--- tipos_de_numeros.py
+++ tipos_de_numeros.py
@@ -1,15 +1,6 @@
 edad = 19
 estatura = 1.80
 n_comp = 5 + 4
-
-
-
-
-
-
-
-
-
 estatura = 1.70
 peso = 70
 imc = peso / (estatura **2)
@@ -42,12 +33,12 @@ print(19 == 19)
 print(bool(0))
 print(bool(1))
 #listas
-ciudades = ["castro", "ancud", "quellon", "chonchi"]
+ciudades = ["castro", "ancud", "quellon", "chonchi", "castro"]
 varios = ["nicolas", 20,True]
 print(type(ciudades))
 print(type(varios))
 #Otra forma de inicializar listas
-lista2 = list("python", "ruby")
+lista2 = list(["python", "ruby"])
 print(ciudades)
 print(type(ciudades))
 #Cantidad de elementos de una lista
@@ -57,10 +48,13 @@ print(ciudades.count("castro"))
 #Impresion de elemento especifico en una lista
 print(ciudades[-3])
 listnum = [1,2,3,4,5,6,7,8,9,10]
-listnum2 = list(range(10))
+listnum2 = list(range(10,1001,10))
 print(listnum2)
-
+#Reasignando valores de listas
+ciudades[0] = "quemchi"
+print(ciudades)
 #Tuplas
+#Las tuplas tiene que ser actualizadas directamente de donde inicia la lista
 musica = tuple()
 generos = ("rock", "blues", "pop") 
 print(generos)
@@ -68,3 +62,33 @@ print(type(generos))
 print(generos[2])
 #Consulta la posicion de un elemento
 print(generos.index("pop"))
+#Cambio de estructura tupla a lista
+tuplita = ("matthew", 200, "univesidad", True)
+print(tuplita)
+tuplita = list(tuplita)
+print("la tupla ahora es de tipo", type(tuplita))
+#tomando un trozo de una tupla
+print(tuplita[0:3])
+#sets connjuntos
+#cada que ocupo add en un set se guarda en cualquier posicion
+conjunto_vacio = set()
+conjunto_x = {"javascript"}
+print(type(conjunto_vacio))
+print(type(conjunto_x))
+colores = {"azul", "rojo", "amarillo", "verde", "violeta"}
+animales = set({"gato",  "perro", "caballo", "hamster"})
+print(colores)  
+print(animales)
+#ingresando un nuevo elemento al set
+colores.add("celeste")
+print(colores)
+#diccionario
+paciente = {
+    nombre = "nelson" ,
+    edad = 30
+    ciudad = "castro"}
+doctor = dict{
+    "nombre" : "elson"
+    "edad" : 40,}
+print(type(doctor))
+print(type(paciente))
